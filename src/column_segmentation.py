3f007734a8ea75fbefa4a306b8a20f3ce4ff2a43
tudhatnad <zoltan20040605@gmail.com>
2024-10-12 17:13:32 +0200

    Lines are separated into letters

--- src/column_segmentation.py
+++ src/column_segmentation.py
@@ -4,38 +4,36 @@ import scipy.signal
 import utility
 import numpy as np
 
-row_color = cv2.imread("../images/rows/row1.png")
-row = cv2.cvtColor(row_color, cv2.COLOR_BGR2GRAY)
-height, width = row.shape
+rowIndex = 1
+while True:
+    row_color = cv2.imread(f"../images/rows/row{rowIndex}.png")
+    print(rowIndex)
+    if row_color is None:
+        break
 
-vertical_projection = []
-for col in range(width):
-    col_data = row[:, col]
+    row = cv2.cvtColor(row_color, cv2.COLOR_BGR2GRAY)
+    height, width = row.shape
 
-    count_black_pixels = 0
-    for pixel in col_data:
-        if pixel == 0:
-            count_black_pixels += 1
+    vertical_projection = utility.vertical_projection(row)
+    min_points = utility.find_local_minimum_points(vertical_projection)
 
-    vertical_projection.append(count_black_pixels)
+    cv2.imshow("row", row_color)
+    letters = []
+    for i in range(1, len(min_points)):
+        letter = row[:, min_points[i-1] + 1:min_points[i] + 1]
+        #cv2.imwrite(f"../images/letters/letter{i}.png", letter)
+        non_white_cols = np.any(letter < 255, axis=0)
+        letter_trimmed = letter[:, non_white_cols]
+        cv2.imwrite(f"../images/letters/row{rowIndex}_letter{i}.png", letter_trimmed)
 
-#Piros vonalak behúzása azért, hogy vizuálisabb legyen
-min_points = utility.find_local_minimum_points(vertical_projection)
-for i in range(len(min_points) - 1):
-    cv2.line(row_color, (min_points[i], 0), (min_points[i], height), (0,0,255), 1)
+    rowIndex = rowIndex + 1
 
-cv2.imshow("row", row_color)
-letters = []
-for i in range(1, len(min_points)):
-    letter = row[:, min_points[i-1] + 1:min_points[i] + 1]
-    #cv2.imwrite(f"../images/letters/letter{i}.png", letter)
-    non_white_cols = np.any(letter < 255, axis=0)
-    letter_trimmed = letter[:, non_white_cols]
-    cv2.imwrite(f"../images/letters/letter{i}.png", letter_trimmed)
+    #Piros vonalak behúzása azért, hogy vizuálisabb legyen
+    #for i in range(len(min_points) - 1):
+    #    cv2.line(row_color, (min_points[i], 0), (min_points[i], height), (0,0,255), 1)
 
-
-plt.plot(vertical_projection)
-plt.title("Horizontal projection")
-plt.xlabel("Index")
-plt.ylabel("Value")
-plt.show()
+    #plt.plot(vertical_projection)
+    #plt.title("Horizontal projection")
+    #plt.xlabel("Index")
+    #plt.ylabel("Value")
+    #plt.show()
--- src/utility.py
+++ src/utility.py
@@ -9,3 +9,18 @@ def find_local_minimum_points(arr):
             arr_minimum_points.append(i)
 
     return arr_minimum_points
+
+def vertical_projection(image):
+    height, width = image.shape
+    vertical_projection = []
+    for col in range(width):
+        col_data = image[:, col]
+
+        count_black_pixels = 0
+        for pixel in col_data:
+            if pixel == 0:
+                count_black_pixels += 1
+
+        vertical_projection.append(count_black_pixels)
+    
+    return vertical_projection
