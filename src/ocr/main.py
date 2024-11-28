c350c152cf99cab3206606326a1bbfe26af6f186
tudhatnad <zoltan20040605@gmail.com>
2024-11-28 19:22:31 +0100

    test

--- src/ocr/main.py
+++ src/ocr/main.py
@@ -2,10 +2,10 @@ import separator.ocr as ocr
 import cv2
 
 def main():
-    OCR = ocr.ocr("../../images/input/test01.jpg", "../images/")
+    OCR = ocr.ocr("../../images/input/test01.png", "../images/")
 
     OCR.binarize().delete_small_components(10).row_segmentation().save_rows("rows/row").letter_segmentation().save_letters("../images/letters/")
-    #OCR.binarize().delete_small_components(10).row_segmentation().letter_segmentation()
+    #OCR.binarize().delete_small_components(10).row_segmentation().letter_segmentation() 
 
     cv2.waitKey(0)
 
