3a8a7d9da98434c759961fde03dafd10afd24fd2
tudhatnad <zoltan20040605@gmail.com>
2024-11-28 19:15:48 +0100

    input file path

--- src/ocr/main.py
+++ src/ocr/main.py
@@ -2,7 +2,7 @@ import separator.ocr as ocr
 import cv2
 
 def main():
-    OCR = ocr.ocr("../images/input/test01.png", "../images/")
+    OCR = ocr.ocr("../../images/input/test01.jpg", "../images/")
 
     OCR.binarize().delete_small_components(10).row_segmentation().save_rows("rows/row").letter_segmentation().save_letters("../images/letters/")
     #OCR.binarize().delete_small_components(10).row_segmentation().letter_segmentation()
